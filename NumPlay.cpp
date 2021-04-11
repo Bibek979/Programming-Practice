2970710eff951e4829d0a89f1578dfd61d2a2841
Bibekananda Besra <bibekmeananda@gmail.com>
2021-04-12 00:33:16 +0530

    This is day 2

---
+++ NumPlay.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<math.h>                        //Library for math e.g, (10^9)
+
+using namespace std;
+
+class NumPlay
+{
+    public:
+        int input;
+        string num[9] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
+        public:
+            NumPlay()
+            {
+                input=0;
+            }
+
+    int InputMethod()
+    {
+        while(1)
+        {
+            cout<<endl<<"Enter an Integer between 1 and 10^9 : \t";
+            cin>>input;
+            cout<<endl;
+
+            if(input>=1&&input<=pow(10.0, 10.0))
+            {
+                return input;
+            }
+            cout<<"Enter a valid input\n";
+        }
+    }
+
+    void output()
+    {
+        if(input>=1&&input<=9)
+        {
+            cout<<num[input-1]<<endl;                    
+        }
+        
+        else
+        {
+            cout<<"Greater than 9\n";
+        }
+    }
+
+};
+
+
+int main()
+{
+    NumPlay obj;
+    while(1)
+    {
+        obj.InputMethod();
+        obj.output();
+    }    
+}
